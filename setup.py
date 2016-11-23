202d1ffa82dd6d0796017c85c7db01d0aea079a7
Matteo Tomassetti <matteo.tomassetti@your.md>
2016-11-23 17:47:49 +0000

    minor

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
+long_description = open('README.md').read()
 
 def setup_package():
 
